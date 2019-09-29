1aa513c114dadc5d8d42a84088589130bb280407
Robert Laws <roblaws@me.com>
2019-09-29 17:01:05 +0300

    update package.json

---
+++ src/components/LessonPlanForm.component.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class LessonPlanForm extends Component {
+
+  render() {
+    return (
+      <div>
+        Form
+      </div>
+    )
+  }
+}
+
+export default LessonPlanForm;
